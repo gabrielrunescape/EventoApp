f0638eabee42a6e30b45f2cc04e2cb6525df9139
GabrielRuneScape <gabrielfilipe@mail.ru>
2016-10-02 18:46:04 -0300

    Inserção de imagens e lista atraves do string-array no actitity_main

--- app/src/main/java/br/com/gabrielrunescape/eventoapp/MainActivity.java
+++ app/src/main/java/br/com/gabrielrunescape/eventoapp/MainActivity.java
@@ -2,6 +2,9 @@ package br.com.gabrielrunescape.eventoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] array = getResources().getStringArray(R.array.array);
+
+        LinearLayout lista = (LinearLayout) findViewById(R.id.LinearLayout);
+
+        for (String vetor : array) {
+            TextView textView = new TextView(this);
+
+            textView.setText(vetor);
+            lista.addView(textView);
+        }
     }
 }
