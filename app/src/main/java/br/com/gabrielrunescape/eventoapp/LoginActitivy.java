ac676c43a17ef8cb8ca6b494ca5f445a7adf3377
GabrielRuneScape <gabrielfilipe@mail.ru>
2016-10-08 22:50:52 -0300

    Alteração no pacote de idiomas e inserção de uma nova activity para registro de usuários

--- app/src/main/java/br/com/gabrielrunescape/eventoapp/LoginActitivy.java
+++ app/src/main/java/br/com/gabrielrunescape/eventoapp/LoginActitivy.java
@@ -1,8 +1,14 @@
 package br.com.gabrielrunescape.eventoapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+import br.com.gabrielrunescape.eventoapp.controllers.LoginController;
 
 public class LoginActitivy extends AppCompatActivity {
 
@@ -11,5 +17,19 @@ public class LoginActitivy extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.login_actitivy);
+
+        EditText etLogin = (EditText) findViewById(R.id.etLogin);
+        EditText etPassword = (EditText) findViewById(R.id.etPassword);
+
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        Button btnSignIn = (Button) findViewById(R.id.btnSignIn);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActitivy.this, SigninActivity.class));
+            }
+        });
+        //LoginController login = new LoginController(etLogin, etPassword, btnLogin, btnSignIn, this);
     }
 }
