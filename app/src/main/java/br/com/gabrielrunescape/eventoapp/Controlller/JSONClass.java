e8a40afc78c2ca63d4f982da7f54db012528cf61
GabrielRuneScape <gabrielfilipe@mail.ru>
2016-10-14 22:27:30 -0300

    Conexão através do protocolo HTTP/HTTPS finalizada. Foi adicionada uma classe de conexão assincrona para realizar o recebimento das informações.

--- app/src/main/java/br/com/gabrielrunescape/eventoapp/Controlller/JSONClass.java
+++ app/src/main/java/br/com/gabrielrunescape/eventoapp/Controlller/JSONClass.java
@@ -1,48 +1,98 @@
 package br.com.gabrielrunescape.eventoapp.Controlller;
 
-import android.util.Log;
-
-import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.BufferedReader;
-import org.json.JSONException;
-import java.io.InputStreamReader;
-import org.apache.http.HttpResponse;
-import java.io.UnsupportedEncodingException;
-import org.apache.http.client.methods.HttpPost;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.app.Activity;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.NetworkOnMainThreadException;
+import android.widget.Toast;
+import android.content.Context;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 /**
  * Created by gabriel on 12/10/16.
  */
 
-public class JSONClass {
+public class JSONClass  {
     InputStream input = null;
     JSONObject jObect = null;
     String json = "";
 
+    // check network connection
+    public boolean isConnected(Context context){
+        ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        Toast toast;
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            toast =  Toast.makeText(context, "Você está conectado!", Toast.LENGTH_LONG);;
+            toast.show();
+
+            return true;
+        } else {
+            toast =  Toast.makeText(context, "Você não está conectado!", Toast.LENGTH_LONG);;
+            toast.show();
+
+            return false;
+        }
+    }
+
     //Recebe sua url
-    public JSONObject getJSONFromUrl(String url) {
-
-        //HTTP request
+    public JSONObject getJSONFromUrl(Context context) {
         try {
-            // default HttpClient
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpPost httpPost = new HttpPost(url);
+            if (isConnected(context)) {
+                String url = "http://192.168.180.135:3000/api/Usuarios";
 
-            HttpResponse httpResponse = httpClient.execute(httpPost);
-            HttpEntity httpEntity = httpResponse.getEntity();
-            input = httpEntity.getContent();
+                HttpClient client = new DefaultHttpClient();
+                HttpResponse resp = client.execute(new HttpGet(url));
 
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+                input = resp.getEntity().getContent();
+
+                if (input != null) {
+                    System.out.println(input.toString());
+                }
+
+                input.close();
+            }
+        } catch (MalformedURLException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (NetworkOnMainThreadException ex) {
+            ex.printStackTrace();
         }
 
-        try {
+
+        /*try {
+            HttpClient client = new DefaultHttpClient();
+            HttpGet get = new HttpGet(url);
+
+            HttpResponse res = client.execute(get);
+            HttpEntity entity = res.getEntity();
+
+            input = entity.getContent();
+            System.out.println(input);
+        } catch (Exception ex) {
+            Toast toast = Toast.makeText(context, ex.getStackTrace().toString(), Toast.LENGTH_LONG);
+            ex.printStackTrace();
+
+            toast.show();
+        }*/
+
+        /*try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(input, "UTF-8"), 8);
             StringBuilder sb = new StringBuilder();
             String line = null;
@@ -64,7 +114,7 @@ public class JSONClass {
             jObect = new JSONObject(json);
         } catch (JSONException e) {
             Log.e("JSON Parser", "Error parsing data " + e.toString());
-        }
+        }*/
 
         // retorna o objeto
         return jObect;
--- app/src/main/java/br/com/gabrielrunescape/eventoapp/LoginActitivy.java
+++ app/src/main/java/br/com/gabrielrunescape/eventoapp/LoginActitivy.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import org.json.JSONObject;
 
+import br.com.gabrielrunescape.eventoapp.Controlller.ConnectionAsync;
 import br.com.gabrielrunescape.eventoapp.Controlller.JSONClass;
 
 public class LoginActitivy extends AppCompatActivity {
@@ -39,12 +40,29 @@ public class LoginActitivy extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                JSONObject contact = null;
-                JSONClass json = new JSONClass();
-                contact = json.getJSONFromUrl("Sua URL aqui");
-
-                System.out.print(contact);
+                if (isConnected()) {
+                    new ConnectionAsync().execute("http://192.168.180.135:3000/api/Usuarios");
+                }
             }
         });
+    }
+
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        Toast toast;
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            toast =  Toast.makeText(this, "Você está conectado!", Toast.LENGTH_LONG);;
+            toast.show();
+
+            return true;
+        } else {
+            toast =  Toast.makeText(this, "Você não está conectado!", Toast.LENGTH_LONG);;
+            toast.show();
+
+            return false;
+        }
     }
 }
